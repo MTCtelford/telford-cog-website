2f3963cbc63f2bf11bc9c8c09e84279806ff5466
MTCtelford <mahanaimcogtelford@gmail.com>
2025-03-08 11:33:21 +0000

    Updated_LeadersList.js

--- src/shared/pages/Leadership/Components/LeadersList.js
+++ src/shared/pages/Leadership/Components/LeadersList.js
@@ -36,7 +36,7 @@ const LeadersList = function (props) {
 <LeaderItem
         image={br_linu_thomas}
         name="Br. Linu Thomas"
-        designation="Youth Secretary"
+        designation=" Administration"
         decription='Son of Mr. V. Thomas and Mrs. Alice Thomas was born in Delhi, India natively from Adoor, India. Though his maternal family is from Pentecostal, he used to go to Marthoma church until the age of 30. In the year of 2016 he went to Masirah, a small island in Oman, where he learned about the truth of baptism and its importance. He is a nurse by profession. He came to the UK in March 2022 and has been a part of Mahanaim CoG, Telford since then. Within this short time itself he has proved to be well determined and passionate towards the work of God. Despite language barriers he is not afraid to proclaim Gods word.
         His wife Merlin Mathew, Jerome (son) and Serah (daughter).'
         socialMedia = {true}
@@ -46,31 +46,66 @@ const LeadersList = function (props) {
 
 <LeaderItem
         image={sr_suja_biju}
-        name="Sis. Suja Cherian"
-        designation="Ladies Secretary"
-        decription='She is basically from Kottayam, India. She did her nursing at Bangalore and migrated to UK in 2000 and has been working in Wirral Teaching NHS Hospital since then. She has secured B.Sc.(Hons.) and Masters in Advanced Nurse Practice from University of Chester and is working as a Band-7 nurse. She is married to Pr. Biju Cherian and is blessed with 2 children, Reuben and Ryan. She has been the Ladies coordinater at Birkenhead Church of God and continue to do the same at Manchester Mahanaim Church of God. She is a prayer warrior and has been a great support to the church growth.'
-        socialMedia = {true}
-        fb='https://www.facebook.com/bijujoseph.cherian'
-        wa='https://wa.me/447886897349'
-      />
+        name="Br. Gijo George"
+        designation="Treasurer"
+        decription=''
+        socialMedia = {false}
+            />
 
 <LeaderItem
         image={sr_neethu_mary_mathew}
-        name="Sis. Neethu Mary Mathew"
-        designation="Ladies Secretary"
-        decription='Born as a child of Mr. and Mrs. Mathew in Palakkad, India. Until her 15 years of age she was part of Syrian Orthodox Church and thereafter God blessed her to find out the true doctrine and hence followed path of Jesus Christ which changed her life. She was an active participant in all the church ministries and served as Sunday school teacher and youth coordinator in her own capacities. She has done her graduation in nursing at Simet College of Nursing, Palakkad. She migrated to UK in 2020 and was blessed to be one of the pioneer member of Mahanaim Church of God, Telford. She is married to Br. Rejoy Stephan and God has blessed them with a boy child, Evan.'
+        name="Sis. Sini Mathew"
+        designation="- Sunday School Coordinator"
+        decription=''
         socialMedia = {true}
         fb='https://www.facebook.com/neethumary.mathew.7'
         wa='https://wa.me/447459495028'
       />
 
-{/* <LeaderItem
-        image={sunday_school_teachers}
-        name="Sundayschool teachers"
-        designation="Sundayschool teachers"
+<LeaderItem
+        image={sr_neethu_mary_mathew}
+        name="Br. Sam Mathai"
+        designation="YPE Secretary"
         decription=''
         socialMedia = {false}
-      /> */}
+        
+      />
+          <LeaderItem
+        image={sr_neethu_mary_mathew}
+        name="Sis. Raji Joby"
+        designation="Ladies Coordinater"
+        decription=''
+        socialMedia = {true}
+        fb='https://www.facebook.com/neethumary.mathew.7'
+        wa='https://wa.me/447459495028'
+      />
+          <LeaderItem
+        image={sr_neethu_mary_mathew}
+        name="Sis.Tinku Rachel Thomas"
+        designation="Prayer Coordinater"
+        decription=''
+        socialMedia = {true}
+        fb='https://www.facebook.com/neethumary.mathew.7'
+        wa='https://wa.me/447459495028'
+      />
+          
+          
+ <LeaderItem
+        image={sunday_school_teachers}
+        name="Br. Tijo Abraham David "
+        designation="Choir Coordinater"
+        decription=''
+        socialMedia = {false}
+      /> 
+<LeaderItem
+        image={sunday_school_teachers}
+        name="Sis. Renu Chandy"
+        designation="Safeguarding Coordinater"
+        decription=''
+        socialMedia = {false}
+      /> 
+
+          
     </div>
   );
 };
